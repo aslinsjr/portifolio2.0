d2cc964a260ecf68825e06e21383b2a445867250
Alexandre Lins <aslinsjr@gmail.com>
2024-10-18 08:27:43 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='app'>
+    <img src="/Captura de tela 2024-10-18 073711.png" alt="" />
+    </div>
+  )
+}
+
+export default App
